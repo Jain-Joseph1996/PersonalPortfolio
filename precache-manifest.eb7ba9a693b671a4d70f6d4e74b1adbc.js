d18324fe022c0eecf4cf076f5b897bea585117b9
jainj <jainj@DESKTOP-0JSU9DK>
2023-03-23 00:25:45 -0400

    Updates

---
+++ precache-manifest.eb7ba9a693b671a4d70f6d4e74b1adbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68947fac57bcf25ae052",
+    "url": "/PersonalPortfolio/static/css/main.88c512b3.chunk.css"
+  },
+  {
+    "revision": "68947fac57bcf25ae052",
+    "url": "/PersonalPortfolio/static/js/main.eb9fc943.chunk.js"
+  },
+  {
+    "revision": "c3cda49bd7d6e22d16ea",
+    "url": "/PersonalPortfolio/static/js/runtime~main.7462302f.js"
+  },
+  {
+    "revision": "8c7bd47dd6ca23e9144f",
+    "url": "/PersonalPortfolio/static/css/2.2c442374.chunk.css"
+  },
+  {
+    "revision": "8c7bd47dd6ca23e9144f",
+    "url": "/PersonalPortfolio/static/js/2.4699d18b.chunk.js"
+  },
+  {
+    "revision": "0df992f81cd45bc01847d3111a312220",
+    "url": "/PersonalPortfolio/index.html"
+  }
+];
